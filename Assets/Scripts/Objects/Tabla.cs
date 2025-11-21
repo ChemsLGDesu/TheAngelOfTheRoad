cb85ee37fd0bb2181a3abb479c889f8d5d83cf79
wassup-999 <sebasquispeperez@gmail.com>
2025-11-20 20:15:48 -0500

    Se comiteo las collisiones con el player y spawn object

--- Assets/Scripts/Objects/Tabla.cs
+++ Assets/Scripts/Objects/Tabla.cs
@@ -1,7 +1,10 @@
 using UnityEngine;
+using static UnityEngine.Rendering.DebugUI;
 
 public class Tabla : MonoBehaviour
 {
+    public static Tabla Instance;
+    public BoxCollider2D boxCollider2D;
     bool hasTouchGround =  false;
     void Start()
     {
@@ -16,7 +19,37 @@ public class Tabla : MonoBehaviour
     }
     private void OnCollisionEnter2D(Collision2D collision)
     {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            boxCollider2D.isTrigger = true;
+        }
+        else
+        {
+            boxCollider2D.isTrigger = false;
+        }
         hasTouchGround = true;
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Ground")
+        {
+            boxCollider2D.isTrigger = false;
+        }
+        if(collision.tag == "Tabla")
+        {
+            boxCollider2D.isTrigger = false;
+        }
+    }
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.tag == "Ground")
+        {
+            boxCollider2D.isTrigger = false;
+        }
+        if (collision.tag == "Tabla")
+        {
+            boxCollider2D.isTrigger = false;
+        }
     }
 
 }
